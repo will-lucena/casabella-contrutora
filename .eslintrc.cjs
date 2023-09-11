0b34e7be3ab0961f6f7671875e14d531e3239ec5
Will Lucena <will.lucen4@gmail.com>
2023-09-11 13:55:25 -0300

    ignore mult-word-component-name lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,20 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 'off'
+      }
+    }
+  ]
 }
