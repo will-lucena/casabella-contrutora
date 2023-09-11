ceb69490c214d7ff516dc3dc0f9811063888f5cf
Will Lucena <will.lucen4@gmail.com>
2023-09-11 13:55:03 -0300

    remove initial files

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <Header />
+    <NavMobile class="nav--mobile" />
+    <NavDesktop class="nav--desktop" />
+  </header>
+</template>
+
+<script setup>
+import Header from '../atoms/Header.vue'
+import NavDesktop from '../molecules/NavDesktop.vue'
+import NavMobile from '../molecules/NavMobile.vue'
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+}
+
+.nav {
+  &--desktop {
+    display: none;
+  }
+
+  &--mobile {
+    display: flex;
+  }
+}
+
+@media only screen and (min-width: 600px) {
+  .nav {
+    &--desktop {
+      display: flex;
+    }
+
+    &--mobile {
+      display: none;
+    }
+  }
+}
+</style>
